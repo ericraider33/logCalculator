680a406248441aa17815910ae9fbfe2b57125d9d
ericraider33 <ericeschenbach@yahoo.com>
2015-05-05 09:44:11 -0400

    Started initial project and included a basic version of Khovanskii's method for ln

---
+++ Calculator/Methods/Khovanskii.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Calculator.Methods
+{
+    public class Khovanskii
+    {
+        public decimal calculate(decimal x, int attempts)
+        {
+            if (x > 3.14m || x < -3.14m)
+                throw new ArgumentException("Range can't be computed");
+
+            decimal z = x - 1;
+            decimal zSquared = z * z;
+            decimal zPlus2 = z + 2;
+            decimal kPart = 0m;
+            for (int k = attempts; k >= 1; k--)
+            {
+                decimal termSquared = k * k;
+                decimal kConstant = 1 + 2 * k;
+                kPart = termSquared * zSquared / (kConstant * zPlus2 - kPart);
+            }
+
+            decimal result = 2 * z / (zPlus2 - kPart);
+            return result;
+        }
+
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator.Methods;
+
+namespace Calculator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Khovanskii cal = new Khovanskii();
+            Console.WriteLine(cal.calculate(2, 1));
+            Console.WriteLine(cal.calculate(2, 2));
+            Console.WriteLine(cal.calculate(2, 3));
+            Console.WriteLine(cal.calculate(2, 4));
+            Console.WriteLine(cal.calculate(2, 5));
+            Console.WriteLine(cal.calculate(2, 6));
+            Console.WriteLine(cal.calculate(2, 7));
+            Console.WriteLine(cal.calculate(2, 8));
+            Console.WriteLine(cal.calculate(2, 9));
+
+            Console.ReadLine();
+        }
+    }
+}
